193bd0ad41d5dd71d6d8301e16b8d8e8f09ef7ff
seilylook <seilylook@naver.com>
2023-02-22 20:47:46 +0900

    FEAT: with API call, get city information using latitude, logtitude

--- App.js
+++ App.js
@@ -1,15 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { View, StyleSheet, Text, ScrollView, Dimensions } from 'react-native';
 const { width: SCREEN_WIDTH } = Dimensions.get('window');
 
+import * as Location from 'expo-location';
+
+const API_KEY = '8825a6d520c75fc4e4c8fd1ea425e0df';
+
+// location: [
+// {"city": "용인시", "country": "대한민국", "district": "보정동",
+// "isoCountryCode": "KR", "name": "죽전로15번길 6", "postalCode": "16897",
+// "region": "경기도", "street": "죽전로15번길", "streetNumber": "6",
+// "subregion": null, "timezone": "Asia/Seoul"
+// }
+// ]
+
 export default function App() {
+  const [city, setCity] = useState('Loading...');
+  const [days, setDays] = useState([]);
+  const [ok, setOk] = useState(true);
+
+  const getWeather = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+
+    if (!granted) {
+      setOk(false);
+    }
+
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+
+    setCity(location[0].city);
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}`
+    );
+    const positions = await response.json();
+
+    console.log(positions);
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, []);
+
   return (
     <View style={styles.container}>
       <StatusBar style='dark' />
       <View style={styles.city}>
-        <Text style={styles.cityname}>city name</Text>
+        <Text style={styles.cityname}>{city}</Text>
       </View>
+
       <ScrollView
         pagingEnabled
         horizontal
